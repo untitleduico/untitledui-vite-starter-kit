8ce50411657976c236f63e62ce717030b717f449
Dilshod Turobov <deebov@yandex.com>
2025-07-22 18:18:14 +0200

    Remove PostCSS

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
